62aa0875ff828b84284e6f5e86fc5c3199ee8a38
chimekinglsey <onlinekingsley@gmail.com>
2024-06-23 23:49:09 +0100

    added append slash, moved corsheaders middleware to correct position in settings

--- goufer/settings.py
+++ goufer/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', # cors middleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # cors middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
