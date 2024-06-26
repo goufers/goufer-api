32abb88c9a29067aee63d341fb633237c0b4b06d
Oyewole Oyediran <woleoyediran@gmail.com>
2024-06-26 14:44:32 +0100

    Commit1

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,4 @@
 from rest_framework_nested import routers
-from .views import CategoryViewSet, DocumentViewSet, LocationViewSet, ReviewsViewSet, SubCategoryViewSet, GoferViewSet, ErrandViewSet
 from pprint import pprint
 from .views import CategoryViewSet, LocationViewSet, ReviewsViewSet, SubCategoryViewSet
 
@@ -8,7 +7,6 @@ router = routers.DefaultRouter()
 # Parent routers
 router.register('categories', CategoryViewSet, basename='çollections')
 router.register('location', LocationViewSet, basename='location')
-router.register('gofers', GoferViewSet, basename='gofer')
 # router.register('gofers', LocationViewSet, basename='gofer')
 # router.register('vendors', LocationViewSet, basename='gofer')
 # router.register('vendors', LocationViewSet, basename='gofer')
@@ -21,6 +19,26 @@ category_router.register('subcategory', SubCategoryViewSet, basename='category_s
 
 
 
+urlpatterns = router.urls + category_router.urls
+
+
+from rest_framework_nested import routers
+from .views import CategoryViewSet, DocumentViewSet, LocationViewSet, ReviewsViewSet, SubCategoryViewSet, GoferViewSet, ErrandViewSet
+
+router = routers.DefaultRouter()
+
+# Parent routers
+router.register('categories', CategoryViewSet, basename='çollections')
+router.register('documents', DocumentViewSet, basename='documents')
+router.register('location', LocationViewSet, basename='location')
+router.register('gofers', GoferViewSet, basename='gofer')
+
+
+# Child routers
+category_router = routers.NestedDefaultRouter(router, 'categories', lookup='category')
+category_router.register('subcategory', SubCategoryViewSet, basename='category_subcategory')
+
+
 gofer_router = routers.NestedDefaultRouter(router, 'gofers', lookup='gofer')
 gofer_router.register('reviews', ReviewsViewSet, basename='gofer_review')
 
@@ -28,7 +46,4 @@ gofer_router = routers.NestedDefaultRouter(router, 'gofers', lookup='gofers')
 gofer_router.register('errands', ErrandViewSet, basename='gofers_errand')
 
 urlpatterns = router.urls + category_router.urls + gofer_router.urls 
-
-
-urlpatterns = router.urls + category_router.urls
 
--- main/views.py
+++ main/views.py
@@ -54,7 +54,7 @@ class VendorDocumentViewSet(ModelViewSet):
             return [IsAdminUser()]
         return [IsAuthenticated()]
     
-    def get_queryset(self):
+    def get_queryset(self): 
         user = self.request.user
         logged_in_vendor = Vendor.objects.only('id').get(custom_user=user)
         if self.request.user.is_staff:
@@ -80,6 +80,94 @@ class ErrandBoyDocumentViewSet(ModelViewSet):
         if self.request.user.is_staff:
             return ErrandBoyDocument.objects.all()
         return ErrandBoyDocument.objects.filter(gofer_id=logged_in_errand_boy)
+    
+    
+class LocationViewSet(ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['state', 'country']
+    search_fields = ['state', 'country']
+    
+    
+    
+class SubCategoryViewSet(ModelViewSet):
+    serializer_class = SubCategorySerializer
+    def get_queryset(self):
+        return SubCategory.objects.filter(category_id=self.kwargs['category_pk'])
+    def get_serializer_context(self):
+        return {'category_id': self.kwargs['category_pk']}
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['name', 'category_id',]
+    search_fields = ['name']
+    
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'DELETE', 'PATCH']:
+            return [IsAdminUser()]
+        return [AllowAny()]
+    
+    
+    
+class ReviewsViewSet(ModelViewSet):
+    def get_queryset(self):
+        return Reviews.objects.filter(gofer_id=self.kwargs['gofer_pk'])
+    serializer_class = ReviewsSerializer
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['user_id', 'gofer_id', 'rating']
+    search_fields = ['gofer_id', 'rating']
+    
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE', 'PATCH']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+    
+    
+
+    
+
+
+
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from main.serializers import CategorySerializer, DocumentSerializer, LocationSerializer, SubCategorySerializer, ReviewsSerializer, ErrandSerializer, GoferSerializer
+from .models import Category, Document, Location, SubCategory, Reviews
+from user.models import Gofer, Errand
+from django_filters.rest_framework import DjangoFilterBackend
+from main.pagination import CustomPagination
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+
+# Create your views here.
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['category_name']
+    search_fields = ['category_name']
+    
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'DELETE', 'PATCH']:
+            return [IsAdminUser()]
+        return [AllowAny()]
+        
+class DocumentViewSet(ModelViewSet):
+    serializer_class = DocumentSerializer
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['document_type']
+    search_fields = ['document_type']
+    
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE', 'PATCH']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Document.objects.all()
+        return Document.objects.filter(custom_user=self.request.user)
     
     
 class LocationViewSet(ModelViewSet):
