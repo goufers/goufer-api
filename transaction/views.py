1590efc32b99d1b268c5d50b74d10157bae0419d
chimekinglsey <onlinekingsley@gmail.com>
2024-06-28 14:16:35 +0100

    winding up, removed unused modules

--- transaction/views.py
+++ transaction/views.py
@@ -4,9 +4,7 @@ from rest_framework import generics, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import (
-    IsAuthenticated, IsAdminUser,
-    IsAuthenticatedOrReadOnly
-    )
+    IsAuthenticated)
 from django.shortcuts import get_object_or_404
 from .models import (
     Wallet, Transaction, Bank, Schedule, ProGofer, Booking, MessagePoster
