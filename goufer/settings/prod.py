55208e094a023663173f0de7f6e35411a0de4c23
Cornelius Ezeh <cornzeh@gmail.com>
2024-07-15 12:56:09 +0100

    Redeployed the app on Heroku and re-provisioned resources

--- goufer/settings/prod.py
+++ goufer/settings/prod.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['goufer-test-11a466ef5e78.herokuapp.com']
+ALLOWED_HOSTS = ['goufer-test-4cd161188b65.herokuapp.com']
 
 
 DATABASES = {
