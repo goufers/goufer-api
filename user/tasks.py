2df5b200dcfe489e4e69892c70c4783a3667022b
Cornelius Ezeh <cornzeh@gmail.com>
2024-07-21 14:05:50 +0100

    Remodified the celery beat schedule task

--- user/tasks.py
+++ user/tasks.py
@@ -1,30 +1,31 @@
 from user.models import Booking
 from celery import shared_task
 from datetime import datetime
-import time
+
 
 @shared_task
 def update_pro_gofers_availability_to_true_when_booking_end_time_expires():
-    bookings = Booking.objects.all()
+    current_time = datetime.now().time().strftime('%H:%M:%S')
+    current_date = datetime.now().date()
+    bookings = Booking.objects.filter(status='accepted', scheduled_date=current_date, to_time__lt=current_time,)
     for booking in bookings: 
-        scheduled_date = datetime.strptime(booking.scheduled_date, '%Y-%m-%d').date()
-        booking_end_time = time.strptime(booking.from_time, '%H:%M:%S')
-        if datetime.now().date() > scheduled_date and time.time() > booking_end_time:
-            booking.pro_gofer.is_available = True 
-            booking.pro_gofer.save()
+        booking.pro_gofer.is_available = True 
+        booking.pro_gofer.save()
+       
+            
         
                 
     
 @shared_task     
 def make_pro_gofers_unavailable_based_on_bookings_start_datetime():
-    bookings = Booking.objects.all()
+    bookings = Booking.objects.filter(status='accepted')
     for booking in bookings:
-        if booking.status == 'accepted':
-            scheduled_date = datetime.strptime(booking.scheduled_date, '%Y-%m-%d').date()
-            booking_end_time = time.strptime(booking.from_time, '%H:%M:%S')
-            if scheduled_date == datetime.now().date() and booking_end_time == time.time():
-                booking.pro_gofer.is_available = False 
-                booking.pro_gofer.save()
+        scheduled_date = datetime.strptime(booking.scheduled_date, '%Y-%m-%d').date()
+        booking_start_time = datetime.strptime(booking.from_time, '%H:%M:%S').time()
+        if scheduled_date == datetime.now().date() and booking_start_time == datetime.now().time().strftime('%H:%M:%S'):
+            booking.pro_gofer.is_available = False 
+            booking.pro_gofer.save()
+                
                 
                 
 
